db356595e083187b361bec33d469b018ab676a75
indhira15 <indhira.ramirez@utec.edu.pe>
2019-03-28 12:11:57 -0500

    newcode

---
+++ code1.py
@@ -0,0 +1,17 @@
+
+palabra=input("Palabra: ")
+all_let={}
+all_num={}
+c=0;
+for i in palabra:
+	if i in all_let:
+		all_num.index(i)=
+	else:
+		all_let.append(i)
+		all_num[c]=1
+	c=c+1
+
+
+			
+		
+	
