e9c40a0f83d109c4e103531a0f9bbca812f04882
indhira15 <indhira.ramirez@utec.edu.pe>
2019-03-28 12:24:27 -0500

    now just counts vocals

--- code1.py
+++ code1.py
@@ -1,17 +1,11 @@
 
 palabra=input("Palabra: ")
-all_let={}
-all_num={}
-c=0;
+vocales=['a','e','i','o','u']
+count=[0,0,0,0,0]
 for i in palabra:
-	if i in all_let:
-		all_num.index(i)=
-	else:
-		all_let.append(i)
-		all_num[c]=1
-	c=c+1
+	if i in vocales:
+		count[vocales.index(i)]=count[vocales.index(i)]+1
 
-
-			
-		
+print(vocales)
+print(count)
 	
